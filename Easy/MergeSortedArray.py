f944338a8694cd824e44cb2c733ed653d9a88cd0
pthambu <pthambu@ncsu.edu>
2023-10-13 13:33:42 -0400

    2nd question

--- Easy/MergeSortedArray.py
+++ Easy/MergeSortedArray.py
@@ -1,4 +1,4 @@
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+'''You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
@@ -26,8 +26,8 @@ Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
-
-  class Solution:
+'''
+class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
